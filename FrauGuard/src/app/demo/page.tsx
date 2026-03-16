1fd07122ad340e19f21254c2d5fad5423ed8cb26
Jendope <jamesanthroitan@gmail.com>
2026-03-17 03:40:23 +0800

    frontend

--- FrauGuard/src/app/demo/page.tsx
+++ FrauGuard/src/app/demo/page.tsx
@@ -1,5 +1,6 @@
-'use client'
 
+'use client'
+import ReactMarkdown from 'react-markdown'
 import { useState } from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
